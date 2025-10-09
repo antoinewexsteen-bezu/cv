e5d2b73efbcb92904a91056c43eba762ce38384f
Antoine Wexsteen-Bézu <wexsteenbezuantoine@gmail.com>
2025-10-09 14:34:17 +0200

    script.js

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ const observer = new IntersectionObserver(entries => {
 }, { threshold: 0.2 });
 
 sections.forEach(section => observer.observe(section));
+
+
+// Accordéon pour les expériences
+const expButtons = document.querySelectorAll('.exp-header');
+
+expButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const parent = btn.parentElement;
+
+    // Ferme les autres si on veut un seul ouvert à la fois
+    expButtons.forEach(other => {
+      if (other !== btn) other.parentElement.classList.remove('active');
+    });
+
+    // Active/désactive l'élément cliqué
+    parent.classList.toggle('active');
+  });
+});
